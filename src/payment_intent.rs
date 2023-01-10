302a0990bdba3e0d0de4452c3ce8becaffe5c984
xEntenKoeniqx <109136581+xEntenKoeniqx@users.noreply.github.com>
2023-01-10 23:20:26 +0100

    Added more response details. Added more documentation

--- src/payment_intent.rs
+++ src/payment_intent.rs
@@ -1,32 +1,98 @@
 use serde::{ Serialize, Deserialize };
 
+/// Portion of the amount that corresponds to a tip.
 #[derive(Serialize, Deserialize)]
 pub struct AmountDetailsTip {
+  /// Portion of the amount that corresponds to a tip.
   pub amount: Option<u32>
 }
 
+/// Details about items included in the amount
 #[derive(Serialize, Deserialize)]
 pub struct AmountDetails {
+  /// Portion of the amount that corresponds to a tip.
   pub tip: AmountDetailsTip
 }
 
+/// Settings to configure compatible payment methods from the [Stripe Dashboard](https://dashboard.stripe.com/settings/payment_methods)
 #[derive(Serialize, Deserialize)]
 pub struct AutomaticPaymentMethods {
+  /// Automatically calculates compatible payment methods
   pub enabled: bool
 }
 
-/// Has to be tested! May crash as some values ​​may be null
+/// **Has to be tested! May crash as some values ​​may be null**
+/// 
+/// The payment error encountered in the previous PaymentIntent confirmation.
+/// It will be cleared if the PaymentIntent is later updated for any reason.
 #[derive(Serialize, Deserialize)]
 pub struct LastPaymentError {
+  /// For card errors, the ID of the failed charge.
   pub charge: String,
+  /// For some errors that could be handled programmatically, a short string indicating the [error code](https://stripe.com/docs/error-codes) reported.
   pub code: String,
+  /// For card errors resulting from a card issuer decline, a short string indicating the [card issuer’s reason for the decline](https://stripe.com/docs/declines#issuer-declines) if they provide one.
   pub decline_code: String,
+  /// A URL to more information about the [error code](https://stripe.com/docs/error-codes) reported.
   pub doc_url: String,
+  /// A human-readable message providing more details about the error.
+  /// For card errors, these messages can be shown to your users.
   pub message: String,
+  /// If the error is parameter-specific, the parameter related to the error.
+  /// For example, you can use this to display a message near the correct form field.
   pub param: String,
   //pub payment_method: ?,
+  /// If the error is specific to the type of payment method, the payment method type that had a problem.
+  /// This field is only populated for invoice-related errors.
   pub payment_method_type: String,
+  /// The type of error returned.
+  /// One of `api_error`, `card_error`, `idempotency_error`, or `invalid_request_error`
   pub r#type: String
+}
+
+/// Shipping address.
+#[derive(Serialize, Deserialize)]
+pub struct ShippingAddress {
+  /// City, district, suburb, town, or village.
+  pub city: String,
+  /// Two-letter country code [(ISO 3166-1 alpha-2)](https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2).
+  pub country: String,
+  /// Address line 1 (e.g., street, PO Box, or company name).
+  pub line1: String,
+  /// Address line 2 (e.g., apartment, suite, unit, or building).
+  pub line2: Option<String>, // can be null?
+  /// ZIP or postal code.
+  pub postal_code: String,
+  /// State, county, province, or region.
+  pub state: String
+}
+
+/// Shipping information for this PaymentIntent.
+#[derive(Serialize, Deserialize)]
+pub struct Shipping {
+  /// Shipping address.
+  pub address: ShippingAddress,
+  /// The delivery service that shipped a physical product, such as Fedex, UPS, USPS, etc.
+  pub carrier: String,
+  /// Recipient name.
+  pub name: String,
+  /// Recipient phone (including extension).
+  pub phone: Option<String>, // can be null?
+  /// The tracking number for a physical product, obtained from the delivery service.
+  /// If multiple tracking numbers were generated for this purchase, please separate them with commas.
+  pub tracking_number: Option<String> // can be null?
+}
+
+/// The data with which to automatically create a Transfer when the payment is finalized. See the PaymentIntents [use case for connected accounts](https://stripe.com/docs/payments/connected-accounts) for details.
+#[derive(Serialize, Deserialize)]
+pub struct TransferData {
+  ///Amount intended to be collected by this PaymentIntent.
+  /// A positive integer representing how much to charge in the [smallest currency unit](https://stripe.com/docs/currencies#zero-decimal) (e.g., 100 cents to charge $1.00 or 100 to charge ¥100, a zero-decimal currency).
+  /// The minimum amount is $0.50 US or [equivalent in charge currency](https://stripe.com/docs/currencies#minimum-and-maximum-charge-amounts).
+  /// The amount value supports up to eight digits (e.g., a value of 99999999 for a USD charge of $999,999.99).
+  pub amount: u32,
+  /// The account (if any) the payment will be attributed to for tax reporting, and where funds from the payment will be transferred to upon payment success.
+  pub destination: Option<String>
 }
 
 /// Payment intent object from 01/08/2023
@@ -66,11 +132,11 @@ pub struct Response {
   pub receipt_email: Option<String>,
   pub review: Option<String>,
   //pub setup_future_usage: ?,
-  //pub shipping: ?,
+  pub shipping: Option<Shipping>,
   pub statement_descriptor: Option<String>,
   pub statement_descriptor_suffix: Option<String>,
   pub status: String,
-  //pub transfer_data: ?,
+  pub transfer_data: Option<TransferData>,
   pub transfer_group: Option<String>
 }
 
