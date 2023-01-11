39771d90454105a2549fa6ef4a88dc8a09c5951b
xEntenKoeniqx <109136581+xEntenKoeniqx@users.noreply.github.com>
2023-01-11 13:09:47 +0100

    Split the structures for payment_intent into their own files

---
+++ src/payment_intent_structs/last_payment_error.rs
@@ -0,0 +1,30 @@
+/// **Has to be tested! May crash as some values ​​may be null**
+/// 
+/// The payment error encountered in the previous PaymentIntent confirmation.
+/// It will be cleared if the PaymentIntent is later updated for any reason.
+/// 
+/// MISSING DETAILS: `payment_method`
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LastPaymentError {
+  /// For card errors, the ID of the failed charge.
+  pub charge: String,
+  /// For some errors that could be handled programmatically, a short string indicating the [error code](https://stripe.com/docs/error-codes) reported.
+  pub code: String,
+  /// For card errors resulting from a card issuer decline, a short string indicating the [card issuer’s reason for the decline](https://stripe.com/docs/declines#issuer-declines) if they provide one.
+  pub decline_code: String,
+  /// A URL to more information about the [error code](https://stripe.com/docs/error-codes) reported.
+  pub doc_url: String,
+  /// A human-readable message providing more details about the error.
+  /// For card errors, these messages can be shown to your users.
+  pub message: String,
+  /// If the error is parameter-specific, the parameter related to the error.
+  /// For example, you can use this to display a message near the correct form field.
+  pub param: String,
+  //pub payment_method: ?,
+  /// If the error is specific to the type of payment method, the payment method type that had a problem.
+  /// This field is only populated for invoice-related errors.
+  pub payment_method_type: String,
+  /// The type of error returned.
+  /// One of `api_error`, `card_error`, `idempotency_error`, or `invalid_request_error`
+  pub r#type: String
+}
